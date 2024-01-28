f3d038f1306fb75024a3f47e7df0a4907d9ac699
GreenFlag31 <unam31@gmail.com>
2024-01-28 13:23:57 +0100

    Auto-generated commit [ci skip]

--- projects/ngx-snackbar-ease/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-snackbar-ease
- */
-
-export * from './lib/snackbar/snackbar.service';
-export * from './lib/snackbar/snackbar.module';
--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'ngx-snackbar-ease' },
-  { path: 'ngx-snackbar-ease', component: MainComponent },
-];
